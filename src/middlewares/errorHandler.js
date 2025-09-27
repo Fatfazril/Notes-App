9c810dff3ae801f3a97f2a45eb6ff7f47fbe6067
Azril Fatfa <azril.fatfa@gmail.com>
2025-09-27 17:35:48 +0700

    add middilwares errorHanlder

---
+++ src/middlewares/errorHandler.js
@@ -0,0 +1,10 @@
+const errorHanlder = (err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+
+    res.status(statusCode).json({
+        status: 'error',
+        statusCode,
+        message
+    });
+}
